d3e10b0ac432cfac1dfdf68843ffcad4b0923412
Jesse Wei <jesse@cs.unc.edu>
2023-05-08 17:12:47 -0400

    Done

--- public/scripts.js
+++ public/scripts.js
@@ -26,15 +26,15 @@ function displayShots() {
 function displayRPSLSOptions() {
     let rpsls_checked = document.getElementById("rpsls").checked;
 
-    let rps_options = document.getElementsByName("rps_option");
-    rps_options.forEach((rps_option) => {
-        rps_option.className = "active";
-    });
+    let rps_shot_options = document.getElementsByName("rps_shot_option");
+    rps_shot_options.forEach(rps_shot_option => {
+        rps_shot_option.className = "active"
+    })
 
-    let rpsls_options = document.getElementsByName("rpsls_option");
-    rpsls_options.forEach((rpsls_option) => {
-        rpsls_option.className = rpsls_checked ? "active" : "inactive";
-    });
+    let rpsls_shot_options = document.getElementsByName("rpsls_shot_option");
+    rpsls_shot_options.forEach(rpsls_shot_option => {
+        rpsls_shot_option.className = rpsls_checked ? "active" : "inactive";
+    })
 }
 
 // Navigation buttons
@@ -42,10 +42,11 @@ function displayRPSLSOptions() {
 /**
  * Play button
  * Disables everything except result.
- * TODO: Implement logic for playing the game.
  */
-function play() {
-    if (!document.getElementById("rps").checked && !document.getElementById("rpsls").checked) {
+async function play() {
+    let rps_checked = document.getElementById("rps").checked;
+    let rpsls_checked = document.getElementById("rpsls").checked;
+    if (!rps_checked && !rpsls_checked) {
         alert("Please select a game mode.");
         throw new RangeError(`Must select game mode before playing.`);
     }
@@ -53,6 +54,35 @@ function play() {
     document.getElementById("shot_options").className = "inactive";
     document.getElementById("result").className = "active";
     document.getElementById("play").className = "inactive";
+
+    let game_mode = rps_checked ? "rps" : "rpsls";
+    let opponent_checked = document.getElementById("opponent").checked;
+    let shot = "";
+    if (opponent_checked) {
+        // name*="shot_option" means name contains "shot_option" as substring
+        // i.e., both rps_shot_option and rpsls_shot_option
+        // Source: https://api.jquery.com/attribute-contains-selector/
+        shot = document.querySelector('input[type="radio"][name*="shot_option"]:checked').value;
+    }
+
+    let api_url = `${document.baseURI}app/${game_mode}/play/${shot}`;
+
+    // Source: https://github.com/jdmar3/coinserver/blob/main/public/main.js line 13
+    await fetch(api_url)
+        .then(function(response) {
+            return response.json();
+        })
+            .then(function(result) {
+                console.log(result);
+                let result_element = document.getElementById("result");
+                if (opponent_checked) {
+                    result_element.innerHTML = `<p>You: ${capitalizeFirstLetter(result.player)}</p>
+                    <p>Your opponent: ${capitalizeFirstLetter(result.opponent)}</p>
+                    <p>Result: You ${result.result.toUpperCase()}</p>`;
+                } else {
+                    result_element.innerHTML = result.player.toUpperCase();
+                }
+            });
 }
 
 /**
@@ -68,13 +98,18 @@ function startOver() {
     // Uncheck all buttons
     document.getElementById("rps").checked = false;
     document.getElementById("rpsls").checked = false;
-    // Uncheck it and trigger the event for unchecking
-    if (document.getElementById("opponent").checked) {
-        document.getElementById("opponent").click();
-    }
+    document.getElementById("opponent").checked = false;
     document.getElementById("rock").checked = false;
     document.getElementById("paper").checked = false;
     document.getElementById("scissors").checked = false;
     document.getElementById("lizard").checked = false;
     document.getElementById("spock").checked = false;
+}
+
+/**
+ * @param {*} string 
+ * @returns string with first letter capitalized
+ */
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
